3c9e5b0a7477a1a09edd7dea5aed865cfe1e0661
tubaaNur <yesiltastubanur@gmail.com>
2022-04-04 17:14:38 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+namespace ConsoleApp1
+{
+     class Program
+    {
+        private static void name()
+            {
+            Console.WriteLine("Tuba");
+            Console.WriteLine("Başar");
+            Console.WriteLine("Özge");
+        }
+        static void Main(string [] args)
+        {
+            name();
+            name();
+            Console.Read();
+        }
+
+
+    }
+}
