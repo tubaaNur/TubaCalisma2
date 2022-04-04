ec4068d779c70259ddf975f37cec554d0b12bc57
tubaaNur <yesiltastubanur@gmail.com>
2022-04-04 17:32:36 +0300

    Base kullanımı

---
+++ ConsoleApp1/A1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TubaCalisma2
+{
+    class A1
+    {
+        public void NameWrite()
+        {
+            Console.WriteLine("Ad: ");
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,23 +3,35 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using System.Linq;
+using TubaCalisma2;
+
 namespace ConsoleApp1
 {
-     class Program
+    class Program
     {
         private static void name()
-            {
+        {
             Console.WriteLine("Tuba");
             Console.WriteLine("Başar");
             Console.WriteLine("Özge");
         }
-        static void Main(string [] args)
+        private static void AgeWrite(int age)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                Console.WriteLine(age);
+            }
+        }
+        static void Main(string[] args)
         {
             name();
             name();
             Console.Read();
+
+            A2 a2 = new A2();
+            a2.AgeWrite();
+           
+
         }
-
-
     }
 }
